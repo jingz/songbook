49d02236799dfd65221dfb690e44a824c8237a44
Sarunyoo Wangwong <wsaryoo@gmail.com>
2012-02-19 00:13:08 +0700

    add recent feature

---
+++ db.js
@@ -0,0 +1,68 @@
+try	{
+	var db = window.openDatabase('chordtab','1.0','chordtab', 2*1024*1024);
+	db.transaction(function  (tx) {
+		tx.executeSql("create table songs(song_id unique,song_name,has_tab BOOLEAN,song_group,artist,alblum,latest_playing DATETIME)");
+		console.log("create song database;")
+	},function  (error) {
+		console.log(error.message)
+	});
+
+	// util fuction
+	db.query = function  (sql, params, cb_rec, cb_err) {
+			db.transaction(function  (tx) {
+				 tx.executeSql(sql,params,function (tx,results) {
+					  var records = [];
+					  if(results){
+							for (var i = 0; i < results.rows.length; i++) {
+								 records.push(results.rows.item(i))
+							};
+						}
+						if(typeof cb_rec == "function") cb_rec(records)
+				 })
+			},function  (error) {
+				if(typeof cb_err == "function") cb_err(error)
+			})
+	}
+}
+catch(e){
+	console.log(e)
+}
+	// db already created
+var Song = {
+	save: function (obj,cb_err) {
+		 // validate
+		 var data = $.extend({song_id:false,song_name:false,has_tab:false},obj)
+		 if(data.id == false || data.name == false){
+			 alert("invalid song id or song name");
+			 return false;
+		 }
+		 var res = db.query(
+				"INSERT INTO songs(song_id, song_name, has_tab, song_group, artist, alblum, latest_playing) values (?,?,?,?,?,?,?)",
+				[data.song_id, data.song_name, data.has_tab, data.song_group, data.artist, data.alblum, Date()],
+				function  (rec) {
+					console.log(rec)
+				},
+				function  (err) {
+					if(typeof cb_err == "function") cb_err(err.message)
+				}
+		 )
+		 console.log(res)
+	},
+
+	update_if_exist_or_save: function  (obj) {
+		 // check whether song has alrealy exist
+		 this.save(obj,function  (err) {
+			  console.log(err.message)	
+        db.query("update songs set latest_playing = ? where song_id = ?",[Date(), obj.song_id])
+		 });
+	},
+
+	all: function (cb_rec) {
+		 db.query("select * from songs order by latest_playing desc limit 50",[],cb_rec) 
+	},
+
+	_where: [],
+	where: function  (sql, params) {
+		this._where.push(sql)
+	}
+}
---
+++ song_list_controller.js
@@ -0,0 +1,132 @@
+var SongListController = {
+	action: {
+			update_seachbox: function(s,e){
+				// update sytle of selected song in the list
+				$("#song-viewport").find("*").removeClass("song-selected")
+				$(e.target).parent().addClass("song-selected")
+				
+				$("#q").val($(e.target).text())
+				$("#song-viewport").slideUp(500)
+			},
+
+			fetch_chord: function(s,e){
+				// Save recent playing song
+				Song.update_if_exist_or_save(s)
+				this.update_seachbox(s,e);
+				var song_id = s.song_id
+				$.ajax({
+					url: CHORD_URI + song_id,
+					data: 'xjxfun=getChord&xjxargs[]=' + encodeURI(song_id),
+					type:'post',
+					error: function(e){ console.log("fetch chord error") },
+					beforeSend: function(){
+						Flash.show({message: "♫ ... ♫",persist: true})
+					},
+					success: function(_res){
+						Flash.close()
+						// reset result viewport to show chord and tab
+
+						$("#chord").html('');
+						$("#main").html('');
+						$("#tab").html('');
+
+						// fetch tab if has tab in the song, async
+						if(s.has_tab){
+							$.ajax({
+								url: CHORD_URI + song_id,
+								data: 'xjxfun=getTabById&xjxargs[]=' + encodeURI(song_id) + "&xjxargs[]=Guitar&xjxargs[]=0",
+								type: 'post',
+								success: function(res){
+									var content = $(res.documentElement.textContent)
+									var tab = $("#tab")
+									// tab.append($(content))
+									content.find('*').removeAttr('style')
+									// TODO hard code
+									var imgs = content.find("img")
+									if(imgs.length == 0){
+											// it maybe flash		
+											var flash = content.find("object")
+											if(flash.length > 0){
+												flash.each(function(){
+													var embed = $(this).find("embed")
+													var param = $(this).find("param")
+													var src = embed.attr("src")
+													var val = param.attr("value")
+													embed.attr("src",src.replace(".",CHORDTABS_URI))
+													param.attr("value",val.replace(".",CHORDTABS_URI))
+												});
+											}
+											else{
+												Flash.show({ message:"Not Found! Why?"})
+											}
+									}
+									else{
+										imgs.each(function(){
+											var src = $(this).attr("src")
+											$(this).attr("src",src.replace(".",CHORDTABS_URI))
+											// $(this).parent().addClass("tab")
+										})
+									}
+
+									tab.append(content)
+									tab.find("*").removeAttr('style')
+									tab.find('table').width("720px")
+								} // end success
+								
+							})
+						}
+
+						// upto chord closure
+						var content = _res.documentElement.textContent
+						// get image result first
+						var imgs = $(content).find("img")
+						if(imgs.length === 0){
+							// maybe flash
+							// try to get flash
+							// ex cash jason mraz song
+							var flash = $(content).find("object")
+							if(flash.length > 0){
+								flash.each(function(){
+									var embed = $(this).find("embed")
+									var src = embed.attr("src")
+									embed.attr("src",src.replace(".",CHORDTABS_URI))
+									$("#chord").append($(this))
+								});
+							}
+							else{
+								Flash.show({ message:" Not found ! Why ?"})
+							}
+						} // end if image.length === 0
+						else {
+							imgs.each(function(){
+								var src = $(this).attr("src");
+								var chords = [], main;
+								$(this).attr("src",src.replace(".",CHORDTABS_URI))
+								if(src.match(/song/)){
+									$(this).appendTo($("#main"))
+								} else{
+									$(this).appendTo($("#chord"))
+								}
+							})
+						}
+						// hardcode style
+						$("#chord,#main").find("img").css({"border-radius": "0.5em",borderColor: "#555"})
+					}
+				})
+				return false; // disable event bubble
+			}
+	},
+
+	binding: function  (data) {
+		ko.applyBindings($.extend({
+			songs: data
+		},this.action))
+	},
+
+	fetch_recent: function(){
+		var self = this;
+		Song.all(function(rec){
+				if(rec.length > 0) self.binding(rec)
+		})	
+	}
+}
